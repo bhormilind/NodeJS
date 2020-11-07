92e8ea08c70b74340351e213be63b108a7fd5934
Milind Bhor <bhormilind@gmail.com>
2020-11-07 18:57:22 -0500

    Added Body Parser and getting input from form

--- projects/ExpressDemo/main.js
+++ projects/ExpressDemo/main.js
@@ -1,13 +1,25 @@
 const express =require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req,res,next) => {
+app.use(bodyParser.urlencoded());
+
+app.use('/about-us',(req,res,next) => {
     console.log('First middle ware');
-    next();
+    res.send('We are a century old busineess');
 });
 
-app.use((req,res,next) => {
+app.use('/add-product',(req,res,next) => {
+    res.send('<form action="/product" method="POST"> <input type="text" name="title"><button type="Submit" name="ok">OK</button></form>');
+});
+
+app.use('/product',(req,res,next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+app.use('/',(req,res,next) => {
+    console.log('Second middle ware');
     res.send('Hello ');
 })
 
