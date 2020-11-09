ac6c16d277150bb1336f794d257a547b7c4ab877
Milind Bhor <bhormilind@gmail.com>
2020-11-08 20:09:41 -0500

    Sending Html file back as a response

--- projects/ExpressRoutingDemo/routes/shopping.js
+++ projects/ExpressRoutingDemo/routes/shopping.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 
 router.get('/',(req,res,next) => {
-    res.send('Welcome to routing express demo ');
+    res.sendFile(path.join(__dirname,'../','views','home.html'));
+    // __dirname :- Present Directory
+    // ../ :- go one level up
+    // views :- go to 'views' directory 
+    // home.html :- sending home.html page
 });
 
 
